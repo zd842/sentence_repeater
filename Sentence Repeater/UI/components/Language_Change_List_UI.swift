8f797405bbc9a4f8f9e56c1ef73519241820f804
zhi-xun <terry20021010@gmail.com>
2022-11-26 22:11:14 +0000

    add language_Change_lise file

---
+++ Sentence Repeater/UI/components/Language_Change_List_UI.swift
@@ -0,0 +1,20 @@
+//
+//  Language_Change_List_UI.swift
+//  Sentence Repeater
+//
+//  Created by macos on 26/11/2022.
+//
+
+import SwiftUI
+
+struct Language_Change_List_UI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Language_Change_List_UI_Previews: PreviewProvider {
+    static var previews: some View {
+        Language_Change_List_UI()
+    }
+}
