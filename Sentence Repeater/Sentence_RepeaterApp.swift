fd8e7971a13b602bd56ad523cd9046e6c2245d0c
zhi-xun <terry20021010@gmail.com>
2022-11-26 20:32:47 +0000

    initially stated

---
+++ Sentence Repeater/Sentence_RepeaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sentence_RepeaterApp.swift
+//  Sentence Repeater
+//
+//  Created by macos on 26/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Sentence_RepeaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
