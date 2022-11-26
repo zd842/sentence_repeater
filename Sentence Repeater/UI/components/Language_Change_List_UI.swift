75f13fac0e755799978a2d761ab47e5cac6a6cb7
zhi-xun <terry20021010@gmail.com>
2022-11-26 22:36:52 +0000

    nothing_1

--- Sentence Repeater/UI/components/Language_Change_List_UI.swift
+++ Sentence Repeater/UI/components/Language_Change_List_UI.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct Language_Change_List_UI: View {
+    
     enum Language_option: String, CaseIterable, Identifiable {
         case Japanese, English, Taiwanese
         var id: Self { self }
     }
     
-    @State private var selectedFlavor: Language_option = .Japanese
+    enum Language_option: String, CaseIterable, Identifiable {
+        case Japanese, English, Taiwanese
+        var id: Self { self }
+    }
+    
+    @State private var selectedSpeed: Language_option = .Japanese
+    @State private var selectedLanguage: Language_option = .Japanese
+    
     var body: some View {
         List {
+            Picker("Language", selection: $selectedFlavor) {
+                Text("Japanese").tag(Language_option.Japanese)
+                Text("English").tag(Language_option.English)
+                Text("Taiwanese").tag(Language_option.Taiwanese)
+            }
+            Spacer()
             Picker("Language", selection: $selectedFlavor) {
                 Text("Japanese").tag(Language_option.Japanese)
                 Text("English").tag(Language_option.English)
