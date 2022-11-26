b9354ede13dc97ff7f98905462fbaaf9e1ce5316
zhi-xun <terry20021010@gmail.com>
2022-11-26 22:50:32 +0000

    nothing_1

--- Sentence Repeater/UI/components/Language_Change_List_UI.swift
+++ Sentence Repeater/UI/components/Language_Change_List_UI.swift
@@ -14,12 +14,18 @@ struct Language_Change_List_UI: View {
         var id: Self { self }
     }
     
+    enum Language_voice: String, CaseIterable, Identifiable {
+        case jap_1, jap_2, eng_1, eng_2, tw_1
+        var id: Self { self }
+    }
+    
     enum Word_up: String, CaseIterable, Identifiable {
         case s0_9, s1_0, s1_2, s1_5, s2_0
         var id: Self { self }
     }
     
     @State private var selectedLanguage: Language_option = .Japanese
+    @State private var selectedVoice: Language_voice = .jap_1
     @State private var selectedSpeed: Word_up = .s0_9
     
     var body: some View {
@@ -29,10 +35,26 @@ struct Language_Change_List_UI: View {
                 Text("English").tag(Language_option.English)
                 Text("Taiwanese").tag(Language_option.Taiwanese)
             }
-            Picker("Speed", selection: $selectedSpeed) {
+            Picker("Language", selection: $selectedVoice) {
+                if selectedLanguage == .English {
+                    Text("eng_1").tag(Language_voice.eng_1)
+                    Text("eng_2").tag(Language_voice.eng_2)
+                } else if selectedLanguage == .Japanese {
+                    Text("jap_1").tag(Language_voice.jap_1)
+                    Text("jap_2").tag(Language_voice.jap_2)
+                } else {
+                    Text("tw_1").tag(Language_voice.tw_1)
+                }
                 Text("Japanese").tag(Language_option.Japanese)
                 Text("English").tag(Language_option.English)
                 Text("Taiwanese").tag(Language_option.Taiwanese)
+            }
+            Picker("Speed", selection: $selectedSpeed) {
+                Text("0.9").tag(Word_up.s0_9)
+                Text("1.0").tag(Word_up.s1_0)
+                Text("1.2").tag(Word_up.s1_2)
+                Text("1.5").tag(Word_up.s1_5)
+                Text("2.0").tag(Word_up.s2_0)
             }
         }
     }
