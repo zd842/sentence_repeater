56a045fb96893ee1306416b316fb64c53b86cde6
zhi-xun <terry20021010@gmail.com>
2022-11-26 21:43:48 +0000

    add text file

---
+++ Sentence Repeater/Sentence_Content_UI.swift
@@ -0,0 +1,22 @@
+//
+//  Sentence_Content_UI.swift
+//  Sentence Repeater
+//
+//  Created by macos on 26/11/2022.
+//
+
+import SwiftUI
+
+struct Sentence_Content_UI: View {
+    @State private var sentence_content: String = ""
+    var body: some View {
+        TextField("input a sentence", text: $sentence_content)
+            .textFieldStyle(.roundedBorder)
+    }
+}
+
+struct Sentence_Content_UI_Previews: PreviewProvider {
+    static var previews: some View {
+        Sentence_Content_UI()
+    }
+}
--- Sentence Repeater/Sentence_RepeaterApp.swift
+++ Sentence Repeater/Sentence_RepeaterApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Sentence_RepeaterApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Sentence_Content_UI()
         }
     }
 }
